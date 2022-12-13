bf4b3c7fb7c42d68a151580a645daf0862eabc74
kyongha <magpia001@gmail.com>
2022-12-13 09:11:21 +0900

    post_list.htnml, post_detail.html에 start 부트스트랩 템플릿UI 적용

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 app_name="blog"
 urlpatterns = [
-    # ''
+    # '/blog/'
     path('', views.PostList.as_view(), name='post_list'),
+    # '/blog/1/'
+    path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -6,3 +6,8 @@ class PostList(ListView):
     model = Post
     # template_name = 'blog/post_list.html'  
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+    # blog/post_detail.html : 클래스이름_detail.html은 template_name 값 생략 가능
+    # template_name = 'blog/post_detail.html'  
